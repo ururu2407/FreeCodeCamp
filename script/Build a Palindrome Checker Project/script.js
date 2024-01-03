f1c33cc3c57d6ebd2eb78e7c0c8ba24042e3a477
ururu2407 <122101396+ururu2407@users.noreply.github.com>
2024-01-03 19:53:33 +0200

    sixth

---
+++ script/Build a Palindrome Checker Project/script.js
@@ -0,0 +1,34 @@
+const inputText = document.getElementById('text-input');
+const buttonCheck = document.getElementById('check-btn');
+const result = document.getElementById('result');
+
+const checkPalindrome = (input) => {
+    const original = input;
+    if (input === '') {
+        alert('Please input a value')
+        return
+    }
+
+    result.replaceChildren()
+    const changedStr = input.replace(/\W|_/g, '',).toLowerCase();
+    let resultText
+    if (changedStr === [...changedStr].reverse().join('')) {
+        resultText = `<strong>${original}</strong> is a palindrome.`
+    } else {
+        resultText = `<strong>${original}</strong> is not a palindrome.`
+    }
+    const p = document.createElement('p')
+    p.className = 'result-text'
+    p.innerHTML = resultText
+    result.appendChild(p);
+}
+buttonCheck.addEventListener("click", () => {
+    checkPalindrome(inputText.value)
+    inputText.value = '';
+})
+inputText.addEventListener("keydown", e => {
+    if (e.key === 'Enter') {
+        checkPalindrome(inputText.value);
+        inputText.value = '';
+    }
+})
